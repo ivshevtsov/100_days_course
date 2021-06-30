f3f7c6fe3270ce16d1c170db17989073179699d8
ishevcov <shevtsow.ivan@yandex.ru>
2021-06-30 16:01:51 +0300

    First day

---
+++ Day_1_printing-start.py
@@ -0,0 +1,46 @@
+#06
+print('')
+print('Day 1 - python Print Function')
+print('The function is declared like this:')
+print("print('whats to print')")
+
+#07
+print('')
+print('Hello world\nHello '
+'world\nHello world')
+print('Hello'+' ' + 'Guess')
+
+#08
+print('')
+print('Day 1 - String Manipulation')
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print('New lines can be created with a backslash and n.')
+
+#09
+#print('Hello,' + input('What is your name?\n'))
+
+#10
+
+#print('Number of characters: ' + str(len(input('What is your name?\n'))))
+
+#12
+'''
+a = input('a:')
+b = input('b:')
+c=a
+a=b
+b=c
+print('a:'+ a)
+print('b:'+ b)
+'''
+
+#Final progect
+
+City = input('Where are you live?\n')
+Pet_name = input('What name is your pet?\n')
+print('Rock band name is:'+City+' '+Pet_name)
+
+
+
+
